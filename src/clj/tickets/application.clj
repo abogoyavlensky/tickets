47d77af854bfa9d55d7395f19f6e52d53820385e
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2021-09-13 12:55:39 +0300

    Remove server info component

--- src/clj/tickets/application.clj
+++ src/clj/tickets/application.clj
@@ -1,7 +1,6 @@
 (ns tickets.application
   (:gen-class)
   (:require [com.stuartsierra.component :as component]
-            [tickets.components.server-info :refer [server-info]]
             [system.components.endpoint :refer [new-endpoint]]
             [system.components.handler :refer [new-handler]]
             [system.components.middleware :refer [new-middleware]]
@@ -11,8 +10,7 @@
             [tickets.components.db :as db]))
 
 
-(defn app-system
-  [config]
+(defn app-system [config]
   (component/system-map
     :db (db/db-component (:db config))
     :routes     (-> (new-endpoint home-routes)
@@ -21,12 +19,10 @@
     :handler    (-> (new-handler)
                     (component/using [:routes :middleware]))
     :http       (-> (jetty/new-jetty :port (:http-port config))
-                    (component/using [:handler]))
-    :server-info (server-info (:http-port config))))
+                    (component/using [:handler]))))
 
 
-(defn -main
-  [& _]
+(defn -main [& _]
   (let [config (tickets-config/config)]
     (-> config
         app-system
